b7750ffe584de02508cc3ec40308b847bdf17889
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-08 00:02:35 +0100

    Changed front to match the responses

--- eJam-superhero/src/services/superhero.service.ts
+++ eJam-superhero/src/services/superhero.service.ts
@@ -3,8 +3,9 @@ import { iSuperhero } from "../interfaces/iSuperhero";
 export class SuperheroService {
   async getSuperheroes() {
     const response = await fetch("http://localhost:4000/superhero");
-    const data: iSuperhero[] = await response.json();
-    return data;
+    const data: { statusCode: number; data: iSuperhero[] } =
+      await response.json();
+    return data.data;
   }
 
   async addSuperhero(body: iSuperhero) {
